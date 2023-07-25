b4dabfeca3d41df78f3a546b1a41bb1bd57ef2f6
itsy <mail@mailto.com>
2023-07-25 19:25:17 +0545

    Utils for crypto conversion
    Implement the packages for key conversion from ed25519 to curve 25519
    
    TODO: check if low order key provided for conversion
    Priority: LOW (demo app doesn't need it)

---
+++ go.mod
@@ -0,0 +1,5 @@
+module itsy/ssb
+
+go 1.20
+
+require filippo.io/edwards25519 v1.0.0
---
+++ handshake/internal/extra25519/convert.go
@@ -0,0 +1,33 @@
+package extra25519
+
+import (
+	"crypto/ed25519"
+	"crypto/sha512"
+
+	"filippo.io/edwards25519"
+)
+
+func PrivateKeyToCurve25519(curve25519Private *[32]byte, privateKey ed25519.PrivateKey) {
+	h := sha512.New()
+	h.Write(privateKey[:32])
+	digest := h.Sum(nil)
+
+	digest[0] &= 248
+	digest[31] &= 127
+	digest[31] |= 64
+
+	copy(curve25519Private[:], digest)
+}
+
+func PublicKeyToCurve25519(curve25519Public *[32]byte, publicKey ed25519.PublicKey) bool {
+	// TODO: Check low order
+	edPoint, err := new(edwards25519.Point).SetBytes(publicKey)
+
+	if err != nil {
+		return false
+	}
+
+	copy(curve25519Public[:], edPoint.BytesMontgomery())
+
+	return true
+}
