67badf3c9015438367c8d169e1fc8052ca63f5f4
itsy <mail@mailto.com>
2023-07-26 15:19:52 +0545

    Misc modifications
    modifications on module files and such

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module itsy/ssb
 
 go 1.20
 
-require filippo.io/edwards25519 v1.0.0
+require (
+	filippo.io/edwards25519 v1.0.0
+	golang.org/x/crypto v0.11.0
+)
+
+require golang.org/x/sys v0.10.0 // indirect
--- handshake/internal/extra25519/convert.go
+++ handshake/internal/extra25519/convert.go
@@ -1,12 +1,16 @@
 package extra25519
 
 import (
-	"crypto/ed25519"
 	"crypto/sha512"
+
+	"golang.org/x/crypto/ed25519"
 
 	"filippo.io/edwards25519"
 )
 
+// PrivateKeyToCurve25519 converts an ed25519 private keu to a correspoding
+// curve25519 private key such that the resulting curve25519 public key will equal
+// the result from PublicKeyToCurve25519
 func PrivateKeyToCurve25519(curve25519Private *[32]byte, privateKey ed25519.PrivateKey) {
 	h := sha512.New()
 	h.Write(privateKey[:32])
@@ -19,6 +23,8 @@ func PrivateKeyToCurve25519(curve25519Private *[32]byte, privateKey ed25519.Priv
 	copy(curve25519Private[:], digest)
 }
 
+// PublicKeyToCurve25519 converts an Ed25519 public key into curve25519
+// public key that would be generated from the same private key
 func PublicKeyToCurve25519(curve25519Public *[32]byte, publicKey ed25519.PublicKey) bool {
 	// TODO: Check low order
 	edPoint, err := new(edwards25519.Point).SetBytes(publicKey)
