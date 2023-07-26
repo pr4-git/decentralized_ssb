360c9c8f7982da47af22eba1808d04984be03a6b
itsy <mail@mailto.com>
2023-07-26 19:56:49 +0545

    Add connection wrapper
    build on secretbox abstraction and provide wrapper for network connections
    it's been mostly copy-pasted from ssbc implementation
    earlier implemention has been discarded because of memory access errors
    
    + Also adds a small demo-ish program in main.go

--- go.mod
+++ go.mod
@@ -4,7 +4,15 @@ go 1.20
 
 require (
 	filippo.io/edwards25519 v1.0.0
+	github.com/stretchr/testify v1.8.4
+	go.cryptoscope.co/netwrap v0.1.4
 	golang.org/x/crypto v0.11.0
 )
 
-require golang.org/x/sys v0.10.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,65 +1,99 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"itsy/ssb/handshake"
+	"itsy/ssb/secretstream"
 	"log"
 	"net"
-	"os"
+
+	"go.cryptoscope.co/netwrap"
 )
 
 var appkey = []byte("SHSNet")
 
+var (
+	clientKeys, serverKeys *handshake.EdKeyPair
+	appKey                 []byte
+)
+
+func init() {
+	var err error
+	clientKeys, err = handshake.GenEdKeyPair(nil)
+	if err != nil {
+		log.Fatalf("Unable to create keys for client. (Error: %v)", err)
+	}
+	serverKeys, err = handshake.GenEdKeyPair(nil)
+	if err != nil {
+		log.Fatalf("Unable to create keys for server. (Error: %v)", err)
+	}
+
+	appKP, err := handshake.GenEdKeyPair(nil)
+	if err != nil {
+		log.Fatalf("Unable to create key for application. (Error %v)", err)
+	}
+
+	appKey = appKP.Public
+}
+
 func main() {
-	serverKey, err := handshake.GenEdKeyPair(nil)
+	server, err := secretstream.NewServer(*serverKeys, appKey)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Couldn't create server (Error: %v)", err)
 	}
 
-	clientKey, err := handshake.GenEdKeyPair(nil)
+	listener, err := netwrap.Listen(
+		&net.TCPAddr{
+			IP:   net.IP{127, 0, 0, 1},
+			Port: 8080},
+		server.ListenerWrapper())
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("netwrap couldn't create connection (Error: %v)", err)
 	}
 
-	serverState, err := handshake.NewServerState(appkey, *serverKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-	clientState, err := handshake.NewClientState(appkey, *clientKey, serverKey.Public)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	server, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	clientconn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	serverconn, err := server.Accept()
-	if err != nil {
-		fmt.Println("Error accepting: ", err.Error())
-		os.Exit(1)
-	}
-	fmt.Println("client connected")
-	// server
 	go func() {
-		err := handshake.Server(serverState, serverconn)
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println(err)
+			log.Printf("couldn't accept conn (Error %v)", err)
 		}
-		serverconn.Close()
+		defer conn.Close()
+
+		for {
+			connReader := bufio.NewReader(conn)
+			msg, err := connReader.ReadBytes('\n')
+			if err != nil {
+				log.Fatalf("error reading from connection (Error: %v)", err)
+			}
+			fmt.Printf("client: %s", msg)
+			/// -----------write:
+			conn.Write([]byte("All your bases are belong to us!!\n"))
+		}
 	}()
 
-	// client
-	err = handshake.Client(clientState, clientconn)
+	client, err := secretstream.NewClient(*clientKeys, appKey)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("couldn't create client (Error: %v)", err)
 	}
-	clientconn.Close()
+
+	tcpAddr := netwrap.GetAddr(listener.Addr(), "tcp")
+	connWrap := client.ConnWrapper(serverKeys.Public)
+
+	conn, err := netwrap.Dial(tcpAddr, connWrap)
+	if err != nil {
+		log.Fatalf("couldn't dial server (Error: %v)", err)
+	}
+	defer conn.Close()
+
+	for i := 0; i < 10; i++ {
+		//--- write:
+		conn.Write([]byte("oh naurrr!!!!!!\n"))
+		// ------- read:
+		connReader := bufio.NewReader(conn)
+		msg, err := connReader.ReadBytes('\n')
+		if err != nil {
+			log.Fatalf("error reading from connection (Error: %v)", err)
+		}
+		fmt.Printf("server: %s", msg)
+	}
 }
